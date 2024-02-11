61be88685b77df4c9c06f963de0c5b77bf33932f
Robocorns <robocorns004@gmail.com>
2024-02-12 01:01:43 +0200

    Test1 ss

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test1.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class Test1 extends LinearOpMode {
-    DcMotorEx  fs,fd;
+    DcMotorEx  fs,fd,ss;
     @Override
     public void runOpMode() throws InterruptedException {
 
