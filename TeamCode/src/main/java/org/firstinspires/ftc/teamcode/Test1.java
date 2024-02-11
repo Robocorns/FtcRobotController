03e555c9ce3236e8b611043cef1225784978362d
Robocorns <robocorns004@gmail.com>
2024-02-12 00:55:07 +0200

    Test1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test1.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Test1 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+}
